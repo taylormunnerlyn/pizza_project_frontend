9431679f2dfc4cbeab09f91523f6e707d2a64720
Jesse Wardle <jwardle@izeni.com>
2015-04-08 17:08:31 -0600

    Updated the karma conf file.

--- karma/karma.conf.js
+++ karma/karma.conf.js
@@ -28,6 +28,7 @@
             files: [
                 {pattern: 'src/**/*.js', included: false},
 
+                'node_modules/babel-core/browser-polyfill.js',
                 'node_modules/es6-module-loader/dist/es6-module-loader-sans-promises.src.js',
                 'node_modules/systemjs/dist/system.src.js',
                 'node_modules/systemjs/lib/extension-register.js',
